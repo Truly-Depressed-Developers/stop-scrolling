cbb12ae9c3c2cbb051265bb9a631e7a9c680cacd
Krzysztof Wrona <rubikon02@gmail.com>
2024-01-20 23:12:56 +0100

    Feat: Working chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,60 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import Message from "@/app/chat/message";
 
+
+const defaultMessages = [
+    {
+        text: "Cześć, w czym mogę ci pomóc?",
+        areYouAuthor: false
+    },
+    {
+        text: "Mam problem z ilością czasu spędzanego na social mediach",
+        areYouAuthor: true
+    },
+    {
+        text: "Ok, postarajmy się znaleźć rozwiązanie problemu dopasowane dla ciebie. Czym się interesujesz?",
+        areYouAuthor: false
+    },
+]
+
+const defaultAnswers = [
+    {
+        text: "Brzmi ciekawie. Opracuję dla ciebie plan działania na następny tydzień",
+        areYouAuthor: false
+    },
+]
+
+type Message = {
+    text: string
+    areYouAuthor: boolean
+}
+
 const Chat = () => {
+    const [messages, setMessages] = useState<Message[]>([])
+    const [message, setMessage] = useState('');
+
+    if (messages.length == 0) {
+        setMessages(defaultMessages)
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        setMessages(messages => [...messages, {text: message, areYouAuthor: true}])
+        setMessage('')
+        setTimeout(()=>{
+            setMessages(messages => [...messages, defaultAnswers[0]])
+        }, 2000)
+    };
+
     return (
         <div className="flex h-full flex-col p-4">
             <div className="space-y-4">
@@ -23,15 +72,25 @@ const Chat = () => {
             </div>
             <Separator className="my-4" />
             <div className="flex-grow space-y-4 overflow-y-auto">
-                <Message
-                    message={"Cześć, w czym mogę ci pomóc?"}
-                    areYouAuthor={false}
-                />
-                <Message message={"Mam problem z [...]"} areYouAuthor={true} />
+                {messages.map(message =>
+                    <Message
+                        key={message.text}
+                        message={message.text}
+                        areYouAuthor={message.areYouAuthor}
+                    />
+                )}
             </div>
             <div className="flex w-full items-center space-x-2">
-                <Input type="text" placeholder="Wiadomość" />
-                <Button className="bg-accent" type="submit">
+                <Input
+                    type="text"
+                    placeholder="Wiadomość"
+                    value={message}
+                    onChange={handleInputChange}
+                />
+                <Button
+                    className="bg-accent"
+                    type="submit"
+                    onClick={handleSubmit}>
                     Wyślij
                 </Button>
             </div>
