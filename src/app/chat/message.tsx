cdba65c8870640a0184aaa5a6d48e0c3c7653653
Dominik Sieroń <dominiksieron13@gmail.com>
2024-01-20 22:23:20 +0100

    Feat: Color config

--- src/app/chat/message.tsx
+++ src/app/chat/message.tsx
@@ -1,25 +1,33 @@
 import React from "react";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
 import {
     HoverCard,
     HoverCardContent,
     HoverCardTrigger,
-} from "@/components/ui/hover-card"
+} from "@/components/ui/hover-card";
+import { cn } from "@/lib/utils";
 
 type Props = {
-    message: string
-    areYouAuthor: boolean
-}
+    message: string;
+    areYouAuthor: boolean;
+};
 
 const Message = (props: Props) => {
-    return <div className={props.areYouAuthor ? "flex justify-end" : ""}>
-        <div className="space-y-4 border rounded p-4 w-min min-w-64" style={props.areYouAuthor ? {backgroundColor: '#93c941'} : {}}>
-            <p>{props.message}</p>
+    return (
+        <div className={props.areYouAuthor ? "flex justify-end" : ""}>
+            <div
+                className={cn(
+                    "w-min min-w-64 space-y-4 rounded border p-4",
+                    props.areYouAuthor ? "bg-primary text-white" : "",
+                )}
+            >
+                <p>{props.message}</p>
+            </div>
         </div>
-    </div>
+    );
 };
 
 export default Message;
